773bd8a6144ed0689b3b8102048577a0b2d33dd8
kremenso <kremenso@o2.pl>
2013-01-30 20:32:03 +0100

    admin

--- app/controllers/bigos_events_box/admin/event_options_controller.rb
+++ app/controllers/bigos_events_box/admin/event_options_controller.rb
@@ -8,9 +8,11 @@ module BigosEventsBox
       @event_id = @eo.event_id
       @eo.destroy
 
-      respond_to do |format|
-        format.html { redirect_to bigos_events_box.edit_admin_event_path @event_id }
-      end unless request.xhr?
+      if request.xhr?
+        render :text => "Event Option removed"
+      else
+        redirect_to bigos_events_box.edit_admin_event_path @event_id
+      end
     end
 
   end
